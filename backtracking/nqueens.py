00bac65f7235962f6def7268973d263e40ae523c
Maxwel Dsouza <maxellusionist@gmail.com>
2017-04-02 14:38:39 +0530

    N Queens

---
+++ backtracking/nqueens.py
@@ -0,0 +1,25 @@
+# done
+# need to test
+# epi 16.2
+def is_valid(placement):
+    diagonals = len(set([x - i for i, x in enumerate(placement)])) == len(placement)
+    return len(placement) == len(set(placement)) and diagonals
+
+def increment_last_queen(placement, last):
+    while placement[-1] == last:
+        placement.pop()
+    placement[-1] += 1
+
+def nqueens(n):
+    placement = [0]
+    while True:
+        if len(placement) == n and is_valid(placement):
+            return placement
+        if is_valid(placement):
+            placement.append(0)
+            continue
+        elif increment_last_queen(placement, n-1):
+            continue
+    return placement
+
+print nqueens(8)

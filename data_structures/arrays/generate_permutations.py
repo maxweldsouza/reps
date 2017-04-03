02f1402d1c261900145313b4c6dcf1efa351ecd2
Maxwel Dsouza <maxellusionist@gmail.com>
2017-04-04 02:15:24 +0530

    Generate permutations

---
+++ data_structures/arrays/generate_permutations.py
@@ -0,0 +1,16 @@
+def permutations(arr):
+    while True:
+        arr = arr[:]
+        i = len(arr) - 1
+        while i > 0 and arr[i] < arr[i-1]:
+            i -= 1
+        if i == 0:
+            break
+        j = len(arr) - 1
+        while j > i and arr[j] < arr[i-1]:
+            j -= 1
+        arr[i-1], arr[j] = arr[j], arr[i-1]
+        arr = arr[:i+1] + list(reversed(arr[i+1:]))
+        yield arr
+
+print list(permutations([2, 4, 5, 3]))

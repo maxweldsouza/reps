adc1b8a1ad62ad6b89860bb5f3399f2a9ee63fb9
Maxwel Dsouza <maxellusionist@gmail.com>
2017-04-08 21:05:42 +0530

    Coin change dynamic programming

--- dp/coin_change.py
+++ dp/coin_change.py
@@ -1,4 +1,14 @@
 # http://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
+# https://www.hackerrank.com/challenges/coin-change
+
+def coin_change_dp(coins, amount):
+    table = [[1 if i == 0 else 0 for i in range(amount + 1)] for j in coins]
+    for i, coin in enumerate(coins):
+        for s in range(amount + 1):
+            including = table[i][s-coin] if s-coin >=0 else 0
+            excluding = table[i-1][s]
+            table[i][s] = including + excluding
+    return table[-1][-1]
 
 cache = {}
 def coin_change(coins, amount):
@@ -16,4 +26,4 @@ def coin_change(coins, amount):
     cache[(coins, amount)] = result
     return result
 
-print coin_change([1, 2, 3], 4)
+print coin_change_dp([1, 2, 3], 4)
